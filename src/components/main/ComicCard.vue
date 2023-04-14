dd494db4216ef33f1a4931876c604ec784b49c92
FrancescaLoria <francesca.loria90@hotmail.it>
2023-04-14 18:07:53 +0200

    griglia dinamica ( props )

---
+++ src/components/main/ComicCard.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+  name: "ComicCard",
+  props: {
+    imgUrl: String,
+    title: String,
+  },
+};
+</script>
+
+<template>
+  <div class="card">
+    <img :src="imgUrl" />
+    <div class="name-card">{{ title }}</div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.card {
+  width: calc(100% / 6 - 20px);
+  margin: 10px;
+  color: white;
+
+  img {
+    width: 100%;
+    height: 150px;
+    object-fit: cover;
+  }
+
+  .name-card {
+    text-transform: uppercase;
+    font-size: 12px;
+  }
+}
+</style>
